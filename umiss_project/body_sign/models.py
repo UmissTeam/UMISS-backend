1ceff3090685dbda0ba2a2eeedcf4204fb9b03d5
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2017-04-16 18:17:02 -0300

    Adding model body signal

--- umiss_project/body_sign/models.py
+++ umiss_project/body_sign/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BodySignal(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('auth.User', related_name='bodysignals')
+    type_signal = models.CharField(
+            max_length=100,
+            blank=False,
+            default='Batimentos')
+
+
+    class Meta:
+        ordering = ('created',)
