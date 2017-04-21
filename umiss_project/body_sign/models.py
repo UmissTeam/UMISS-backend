d53ce09ab546fd727c7080d387f26617d38f3b11
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2017-04-20 22:46:03 -0300

    Adding new models from body sign
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- umiss_project/body_sign/models.py
+++ umiss_project/body_sign/models.py
@@ -1,13 +1,42 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class BodySignal(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey('auth.User', related_name='bodysignals')
-    type_signal = models.CharField(
-            max_length=100,
-            blank=False,
-            default='Batimentos')
-
 
     class Meta:
         ordering = ('created',)
+        # abstract = True
+        
+    def __str__(self):
+        return "{0.__class__.__name__} created by: {0.owner}".format(self)
+
+
+class HeartBeats(BodySignal):
+    beats = models.IntegerField(
+        validators=[
+            MaxValueValidator(200),
+            MinValueValidator(0)
+        ]
+    )
+
+    
+class GalvanicResistance(BodySignal):
+    resistance = models.IntegerField(
+        validators=[
+            MaxValueValidator(1000),
+            MinValueValidator(100)
+        ]
+    )
+
+    
+class SkinTemperature(BodySignal):
+    temperature = models.IntegerField(
+        validators=[
+            MaxValueValidator(39),
+            MinValueValidator(32)
+        ]
+    )
+
+    
