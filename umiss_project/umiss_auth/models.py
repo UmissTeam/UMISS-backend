3bfc65feb8bbafb5c4c507d6e6fc5a10f60f8d75
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2017-04-22 20:07:35 -0300

    Adding monitors key
    Signed-off-by: Tiago Assunção <thiagoribeironiquel@hotmail.com>

--- umiss_project/umiss_auth/models.py
+++ umiss_project/umiss_auth/models.py
@@ -7,11 +7,26 @@ class CustomUser(AbstractUser):
         ('patient', 'User Type Pacient'),
         ('monitor', 'User Type Monitor'),
     )
-    user_type = models.CharField(max_length=2,
-                                 choices=type_choices,
-                                 default='monitor')
+
+    user_type = models.CharField(
+        max_length=2,
+        choices=type_choices,
+        default='monitor'
+    )
+
     token = models.CharField(max_length=512, editable=False)
 
+    monitor_users = models.ForeignKey(
+        'umiss_auth.CustomUser',
+        related_name='monitors',
+        on_delete=models.SET_NULL,
+        null=True,
+        limit_choices_to=models.Q(user_type = 'monitor')
+    )
+
     def save(self, *args, **kwargs):
-        self.token = hashlib.sha512(self.token.encode('utf-8')).hexdigest()
+        self.token = hashlib.sha512(
+            self.token.encode('utf-8')
+        ).hexdigest()
+
         super(CustomUser, self).save(*args, **kwargs)
