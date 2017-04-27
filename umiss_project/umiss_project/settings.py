d6513d1dc0ec290397b34a5b21ee9998f019c000
Wilton Rodrigues <wiltonsr94@gmail.com>
2017-04-26 21:42:33 -0300

    Allowing GET request from ember server

--- umiss_project/umiss_project/settings.py
+++ umiss_project/umiss_project/settings.py
@@ -25,18 +25,20 @@ SECRET_KEY = '*huz)(ooodwh&$c-a)hsrn&9-%4@7)c!3!41iu)tc3o^9$+o5u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
+    'rest_framework_json_api',
     'rest_framework',
     'body_sign',
     'umiss_auth',
@@ -49,8 +51,9 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
