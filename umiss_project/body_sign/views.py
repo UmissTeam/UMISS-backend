8b5f0e99cc68ad4ec1c07f46fcf4145276d31659
Tiago Assunção <thiagoribeironiquel@hotmail.com>
2017-05-20 17:07:25 -0300

    Adding restriction to get signals

--- umiss_project/body_sign/views.py
+++ umiss_project/body_sign/views.py
@@ -14,28 +14,39 @@ import body_sign.signals
 class HeartBeatsViewSet(viewsets.ModelViewSet):
     queryset = HeartBeats.objects.all()
     serializer_class = HeartBeatsSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        monitor = self.request.user
+        patient = monitor.monitors.first()
+        return HeartBeats.objects.filter(owner=patient)
 
 class GalvanicResistanceViewSet(viewsets.ModelViewSet):
     queryset = GalvanicResistance.objects.all()
     serializer_class = GalvanicResistanceSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        monitor = self.request.user
+        patient = monitor.monitors.first()
+        return GalvanicResistance.objects.filter(owner=patient)
 
 
 class SkinTemperatureViewSet(viewsets.ModelViewSet):
     queryset = SkinTemperature.objects.all()
     serializer_class = SkinTemperatureSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        monitor = self.request.user
+        patient = monitor.monitors.first()
+        return SkinTemperature.objects.filter(owner=patient)
